e288d9922584d1bd9d55024aa79508c6a805e21c
pmayur <thisispatil@gmail.com>
2020-12-09 18:57:03 +0530

    add: functional tests to check get books

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -23,7 +23,7 @@ suite("Functional Tests", function () {
      */
 
     // UNCOMMENT UPON IMPLEMENTATION
-    /* test("#example Test GET /api/books", function (done) {
+    test("#example Test GET /api/books", function (done) {
         chai.request(server)
             .get("/api/books")
             .end(function (err, res) {
@@ -46,7 +46,7 @@ suite("Functional Tests", function () {
                 );
                 done();
             });
-    }); */
+    });
     /*
      * ----[END of EXAMPLE TEST]----
      */
@@ -102,22 +102,69 @@ suite("Functional Tests", function () {
             }
         );
 
-        // UNCOMMENT UPON IMPLEMENTATION
-        /* suite("GET /api/books => array of books", function () {
+        suite("GET /api/books => array of books", function () {
             test("Test GET /api/books", function (done) {
-                //done();
+                chai.request(server)
+                    .get("/api/books")
+                    .end( function(err, res) {
+                        assert.equal(res.status, 200);
+                        assert.isArray(res.body, "response should be an array");
+                        assert.property(
+                            res.body[0],
+                            "commentcount",
+                            "Books in array should contain commentcount"
+                        );
+                        assert.property(
+                            res.body[0],
+                            "title",
+                            "Books in array should contain title"
+                        );
+                        assert.property(
+                            res.body[0],
+                            "_id",
+                            "Books in array should contain _id"
+                        );
+                        assert.notExists(err);
+                        done();
+                    })
             });
         });
 
         suite("GET /api/books/[id] => book object with [id]", function () {
+            let id;
+
+            beforeEach( async () => {
+                id      = await util.getBookIdFromDatabase();
+            });
+
             test("Test GET /api/books/[id] with id not in db", function (done) {
-                //done();
+
+                id = util.randomString;
+
+                chai.request(server)
+                        .get(`/api/books/${id}`)
+                        .end(function (err, res) {
+                            assert.equal(res.status, 200);
+                            assert.equal(res.text, "no book exists");
+                            assert.notExists(err);
+                            done();
+                        });
             });
 
             test("Test GET /api/books/[id] with valid id in db", function (done) {
-                //done();
+                chai.request(server)
+                        .get(`/api/books/${id}`)
+                        .end(function (err, res) {
+                            assert.equal(res.status, 200);
+                            assert.isArray(res.body.comments);
+                            assert.equal(res.body._id, id);
+                            assert.exists(res.body.title);
+                            assert.exists(res.body.commentcount);
+                            assert.notExists(err);
+                            done();
+                        });
             });
-        }); */
+        });
 
         suite(
             "POST /api/books/[id] => add comment/expect book object with id",
